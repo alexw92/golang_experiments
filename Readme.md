226200ac5551f055785e308037c74c060a5b74be
Alexander Werthmann <werdy@gmx.net>
2022-06-04 15:59:25 +0200

    initial

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Go Experiments
+
+[go by examples](https://gobyexample.com/)
+[interactive go tour](https://go.dev/tour/welcome/1)
---
+++ variables.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a = "initial"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	// default zero
+	var e int
+	fmt.Println(e)
+
+	// The := syntax is shorthand for declaring and initializing a variable, e.g. for var f string = "apple" in this case.
+	f := "apple"
+	fmt.Println(f)
+}
